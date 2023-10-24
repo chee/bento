5560968887b1e665c8dc9fca1f8d7d5053c8a391
chee <yay@chee.party>
2023-10-24 21:50:51 +0100

    add missing const file

---
+++ public/graphics.const.js
@@ -0,0 +1,16 @@
+/**
+ * @readonly
+ * @enum {keyof typeof Screen}
+ */
+export const Screen = {
+	/** @type {"wav"} */
+	wav: "wav",
+	/** @type {"env"} */
+	env: "env",
+	/** @type {"mix"} */
+	mix: "mix",
+	/** @type {"fx"} */
+	fx: "fx"
+}
+
+export const DPI = 3

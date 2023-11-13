5080975130077cda9895c54f0935c5e858b252d5
chee <yay@chee.party>
2023-11-13 08:01:59 +0000

    add new controls to types

--- bento.d.ts
+++ bento.d.ts
@@ -2,8 +2,11 @@ import BentoParty from "./public/elements/party"
 import BentoNav from "./public/elements/nav"
 import BentoMachine from "./public/elements/machine"
 import BentoControlButton, {
-	ButtonControlSpec
-} from "./public/elements/control-button"
+	BentoRecordButton,
+	BentoFlipButton,
+	BentoLoopButton,
+	BentoHearButton
+} from "./public/elements/controls/button"
 import BentoBox from "./public/elements/box"
 import BentoGrid from "./public/elements/grid"
 import BentoGridControls from "./public/elements/grid-controls"
@@ -16,8 +19,11 @@ import BentoScreenSelector from "./public/elements/screen-selector"
 import BentoGridSelector from "./public/elements/grid-selector"
 import BentoTape from "./public/elements/tape"
 import BentoControlPopout, {
-	PopoutControlSpec
-} from "./public/elements/control-popout"
+	BentoLayerTypeSelector,
+	BentoSpeedSelector,
+	BentoLoopSelector
+} from "./public/elements/controls/popout"
+import BentoLayerSelectorChoice from "./public/elements/layer-selector-choice"
 
 declare global {
 	type BentoControl = BentoControlButton | BentoControlPopout
@@ -29,8 +35,16 @@ declare global {
 		"bento-machine": BentoMachine
 		"bento-master-controls": BentoMasterControls
 		"bento-layer-selector": BentoLayerSelector
+		"bento-layer-selector-choice": BentoLayerSelectorChoice
 		"bento-screen-controls": BentoScreenControls
 		"bento-control-button": BentoControlButton
+		"bento-record-button": BentoRecordButton
+		"bento-hear-button": BentoHearButton
+		"bento-flip-button": BentoFlipButton
+		"bento-loop-button": BentoLoopButton
+		"bento-layer-type-selector": BentoLayerTypeSelector
+		"bento-speed-selector": BentoSpeedSelector
+		"bento-loop-selector": BentoLoopSelector
 		"bento-control-popout": BentoControlPopout
 		"bento-screen": BentoScreen
 		"bento-screen-selector": BentoScreenSelector

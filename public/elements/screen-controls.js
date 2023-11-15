4cf616bffc402f3012159e0e78fe4a06189b34e8
chee <yay@chee.party>
2023-11-15 11:58:53 +0000

    remove unreleased type selector

--- public/elements/screen-controls.js
+++ public/elements/screen-controls.js
@@ -26,7 +26,7 @@ export const ScreenControl = /** @type const */ ({
 const ScreenControlSet = /** @type const */ ({
 	sampler: {
 		[Screen.wav]: {
-			sound: [ScreenControl.type, ScreenControl.record],
+			sound: [ScreenControl.record],
 			step: [ScreenControl.flip]
 		},
 		[Screen.key]: [ScreenControl.hear]

9322b8f920eb8c293a7bcee118de61512b3ee402
chee <yay@chee.party>
2024-10-03 18:06:20 +0100

    bump the pump

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
 /// <reference types="@secret-types/serviceworker" />
 // update this when changing the service worker
-const SERVICE_WORKER_VERSION = "v15"
+const SERVICE_WORKER_VERSION = "v16"
 
 const addResourcesToCache = async resources => {
 	const cache = await caches.open(SERVICE_WORKER_VERSION)
